0363d66119e6c653d2a9e9a5cd956c5155e7dfd0
sangwoo <hdsk1234@naver.com>
2026-04-01 23:17:44 +0900

    260331

--- solved_problems/13460.cpp
+++ solved_problems/13460.cpp
@@ -1,199 +1,210 @@
+// tilt 함수를 방향마다 한개씩 총 4개를 만든다.
+// tilt는 기울이는 방향에 더 치우쳐진 녀석부터 굴린다.
+// bfs로 10번까지만 해본다.
+
 #include <iostream>
 #include <queue>
 using namespace std;
 
-struct State { 
-    int Ry;
-    int Rx;
-    int By;
-    int Bx;
-    int prev_command;
-    int move_num;
+char board[10][10];
+// int DIR[4][2] = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
+pair<int, int> R_pos;
+pair<int, int> B_pos;
+
+struct pos {
+    pair<int, int> R;
+    pair<int, int> B; 
+    
+    friend ostream& operator<<(ostream& os, const pos& p) {
+        os << p.R.first << " " << p.R.second << " " << p.B.first << " " << p.B.second;
+        return os;
+    }
 };
 
-int N, M;
-char board[10][10];
-pair <int, int> R;
-pair <int, int> B;
-queue<State> q;
-int dir[4][2] = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} }; // 상, 하, 좌, 우
-
-State tilt_Red(int _dir, State& st) {
-
-    int cur_Ry = st.Ry;
-    int cur_Rx = st.Rx;
-    int cur_By = st.By;
-    int cur_Bx = st.Bx;
-
-    int nxt_Ry;
-    int nxt_Rx;
-
-    while(1) {
-        nxt_Ry = cur_Ry + dir[_dir][0];
-        nxt_Rx = cur_Rx + dir[_dir][1];
-
-        if(nxt_Ry < 0 || nxt_Ry >= N || nxt_Rx < 0 || nxt_Rx >= M) {
-            st = {cur_Ry, cur_Rx, cur_By, cur_Bx, _dir, st.move_num+1};
-            break;
-        }
-        else if(board[nxt_Ry][nxt_Rx] == '#') {
-            st = {cur_Ry, cur_Rx, cur_By, cur_Bx, _dir, st.move_num+1};
-            break;
-        }
-        else if(nxt_Ry == cur_By && nxt_Rx == cur_Bx) {
-            st = {cur_Ry, cur_Rx, cur_By, cur_Bx, _dir, st.move_num+1};
-            break;
-        }
-        else if(board[nxt_Ry][nxt_Rx] == '0') {
-            st = {nxt_Ry, nxt_Rx, cur_By, cur_Bx, _dir, st.move_num+1};
-            break;
-        }
-
-        cur_Ry = nxt_Ry;
-        cur_Rx = nxt_Rx;
-    } // while
-
-    return st;
-} // tilt_Red()
-
-State tilt_Blue(int _dir, State& st) {
-    int cur_Ry = st.Ry;
-    int cur_Rx = st.Rx;
-    int cur_By = st.By;
-    int cur_Bx = st.Bx;
-
-    int nxt_By;
-    int nxt_Bx;
-
-    while(1) {
-        nxt_By = cur_By + dir[_dir][0];
-        nxt_Bx = cur_Bx + dir[_dir][1];
-
-        if(nxt_By < 0 || nxt_By >= N || nxt_Bx < 0 || nxt_Bx >= M) {
-            st = {cur_Ry, cur_Rx, cur_By, cur_Bx, _dir, st.move_num+1};
-            break;
-        }
-        else if(board[nxt_By][nxt_Bx] == '#') {
-            st = {cur_Ry, cur_Rx, cur_By, cur_Bx, _dir, st.move_num+1};
-            break;
-        }
-        else if(nxt_By == cur_Ry && nxt_Bx == cur_Rx) {
-            st = {cur_Ry, cur_Rx, cur_By, cur_Bx, _dir, st.move_num+1};
-            break;
-        }
-        else if(board[nxt_By][nxt_Bx] == '0') {
-            st = {cur_Ry, cur_Rx, nxt_By, nxt_Bx, _dir, st.move_num+1};
-            break;
-        }
-        
-        cur_By = nxt_By;
-        cur_Bx = nxt_Bx;
-    } // while
-
-    return st;
-} // tilt_Blue()
-
-State tilt(int dir, State st) {
-    int Ry = st.Ry;
-    int Rx = st.Rx;
-    int By = st.By;
-    int Bx = st.Bx;
-    int move_num = st.move_num;
+int tilt(pos cur_pos, pos& nxt_pos, int dir) { // 성공(0)/미정(1)/실패(2) 여부 반환
+    int result = 1;
+    int ry = cur_pos.R.first;
+    int rx = cur_pos.R.second;
+    int by = cur_pos.B.first;
+    int bx = cur_pos.B.second;
 
     switch(dir) {
         case 0: // 상
-            if(Ry > By) { // B부터
-                st = tilt_Blue(0, st);
-                st = tilt_Red(0, st);
+            if(ry > by) { // B가 위 -> B부터
+                while(1) { // B
+                    if(by-1 == ry && bx == rx) break; // 기울이기 완료
+                    else if(board[by-1][bx] == 'O') return 2; // 실패
+                    else if(board[by-1][bx] == '.') by--; // 계속 기울이기
+                    else if(board[by-1][bx] == '#') break; // 기울이기 완료
+                } // while
+                while(1) { // R
+                    if(ry-1 == by && rx == bx) break; // 기울이기 완료
+                    else if(board[ry-1][rx] == 'O') return 0; // 성공!
+                    else if(board[ry-1][rx] == '.') ry--; // 계속 기울이기
+                    else if(board[ry-1][rx] == '#') break; // 기울이기 완료
+                } // while
             }
-            else { // R부터
-                st = tilt_Red(0, st);
-                st = tilt_Blue(0, st);
+            else { // R이 위 -> R부터
+                while(1) { // R
+                    if(ry-1 == by && rx == bx) break; // 기울이기 완료
+                    else if(board[ry-1][rx] == 'O') { ry = rx = -1; result = 0; break; } // 성공!
+                    else if(board[ry-1][rx] == '.') ry--; // 계속 기울이기
+                    else if(board[ry-1][rx] == '#') break; // 기울이기 완료
+                } // while
+                while(1) { // B
+                    if(by-1 == ry && bx == rx) break; // 기울이기 완료
+                    else if(board[by-1][bx] == 'O') return 2; // 실패
+                    else if(board[by-1][bx] == '.') by--; // 계속 기울이기
+                    else if(board[by-1][bx] == '#') break; // 기울이기 완료
+                } // while
             }
             break;
         case 1: // 하
-            if(Ry < By) { // B부터
-                st = tilt_Blue(1, st);
-                st = tilt_Red(1, st);
+            if(ry < by) { // B가 아래 -> B부터
+                while(1) { // B
+                    if(by+1 == ry && bx == rx) break; // 기울이기 완료
+                    else if(board[by+1][bx] == 'O') return 2; // 실패
+                    else if(board[by+1][bx] == '.') by++; // 계속 기울이기
+                    else if(board[by+1][bx] == '#') break; // 기울이기 완료
+                } // while
+                while(1) { // R
+                    if(ry+1 == by && rx == bx) break; // 기울이기 완료
+                    else if(board[ry+1][rx] == 'O') return 0; // 성공!
+                    else if(board[ry+1][rx] == '.') ry++; // 계속 기울이기
+                    else if(board[ry+1][rx] == '#') break; // 기울이기 완료
+                } // while
             }
-            else { // R부터
-                st = tilt_Red(1, st);
-                st = tilt_Blue(1, st);
+            else { // R이 아래 -> R부터
+                while(1) { // R
+                    if(ry+1 == by && rx == bx) break; // 기울이기 완료
+                    else if(board[ry+1][rx] == 'O') { ry = rx = -1; result = 0; break; }// 성공!
+                    else if(board[ry+1][rx] == '.') ry++; // 계속 기울이기
+                    else if(board[ry+1][rx] == '#') break; // 기울이기 완료
+                } // while
+                while(1) { // B
+                    if(by+1 == ry && bx == rx) break; // 기울이기 완료
+                    else if(board[by+1][bx] == 'O') return 2; // 실패
+                    else if(board[by+1][bx] == '.') by++; // 계속 기울이기
+                    else if(board[by+1][bx] == '#') break; // 기울이기 완료
+                } // while
             }
             break;
         case 2: // 좌
-            if(Rx > Bx) { // B부터
-                st = tilt_Blue(2, st);
-                st = tilt_Red(2, st);
+            if(rx < bx) { // R이 왼쪽 -> R부터
+                while(1) { // R
+                    if(ry == by && rx-1 == bx) break; // 기울이기 완료
+                    else if(board[ry][rx-1] == 'O') { ry = rx = -1; result = 0; break; } // 성공!
+                    else if(board[ry][rx-1] == '.') rx--; // 계속 기울이기
+                    else if(board[ry][rx-1] == '#') break; // 기울이기 완료
+                } // while
+                while(1) { // B
+                    if(by == ry && bx-1 == rx) { break; } // 기울이기 완료
+                    else if(board[by][bx-1] == 'O') return 2; // 실패
+                    else if(board[by][bx-1] == '.') bx--; // 계속 기울이기
+                    else if(board[by][bx-1] == '#') break; // 기울이기 완료
+                } // while
             }
-            else { // R부터
-                st = tilt_Red(2, st);
-                st = tilt_Blue(2, st);
+            else { // B가 왼쪽 -> B부터
+                while(1) { // B
+                    if(by == ry && bx-1 == rx) break; // 기울이기 완료
+                    else if(board[by][bx-1] == 'O') return 2; // 실패
+                    else if(board[by][bx-1] == '.') bx--; // 계속 기울이기
+                    else if(board[by][bx-1] == '#') break; // 기울이기 완료
+                } // while
+                while(1) { // R
+                    if(ry == by && rx-1 == bx) break; // 기울이기 완료
+                    else if(board[ry][rx-1] == 'O') return 0; // 성공!
+                    else if(board[ry][rx-1] == '.') rx--; // 계속 기울이기
+                    else if(board[ry][rx-1] == '#') break; // 기울이기 완료
+                } // while
             }
             break;
         case 3: // 우
-            if(Rx < Bx) { // B부터
-                st = tilt_Blue(3, st);
-                st = tilt_Red(3, st);
+            if(rx > bx) { // R이 오른쪽 -> R부터
+                while(1) { // R
+                    if(ry == by && rx+1 == bx) break; // 기울이기 완료
+                    else if(board[ry][rx+1] == 'O') { ry = rx = -1; result = 0; break; } // 성공!
+                    else if(board[ry][rx+1] == '.') rx++; // 계속 기울이기
+                    else if(board[ry][rx+1] == '#') break; // 기울이기 완료
+                } // while
+                while(1) { // B
+                    if(by == ry && bx+1 == rx) break; // 기울이기 완료
+                    else if(board[by][bx+1] == 'O') return 2; // 실패
+                    else if(board[by][bx+1] == '.') bx++; // 계속 기울이기
+                    else if(board[by][bx+1] == '#') break; // 기울이기 완료
+                } // while
             }
-            else { // R부터
-                st = tilt_Red(3, st);
-                st = tilt_Blue(3, st);
+            else { // B가 오른쪽 -> B부터
+                while(1) { // B
+                    if(by == ry && bx+1 == rx) break; // 기울이기 완료
+                    else if(board[by][bx+1] == 'O') return 2; // 실패
+                    else if(board[by][bx+1] == '.') bx++; // 계속 기울이기
+                    else if(board[by][bx+1] == '#') break; // 기울이기 완료
+                } // while
+                while(1) { // R
+                    if(ry == by && rx+1 == bx) break; // 기울이기 완료
+                    else if(board[ry][rx+1] == 'O') return 0; // 성공!
+                    else if(board[ry][rx+1] == '.') rx++; // 계속 기울이기
+                    else if(board[ry][rx+1] == '#') break; // 기울이기 완료
+                } // while
             }
             break;
-    }
+    } // switch
 
-    return st;
+    nxt_pos = {{ry, rx}, {by, bx}};
 
-} // tilt()
-
-int bfs(State st) {
-    q.push(st);
-    while(!q.empty()) {
-        State cur_st = q.front();
-
-        for(int i = 0; i <= 3; i++) {
-            if(cur_st.prev_command == i) continue;
-            State nxt_st = tilt(i, cur_st);
-            int Ry = nxt_st.Ry;
-            int Rx = nxt_st.Rx;
-            int By = nxt_st.By;
-            int Bx = nxt_st.Bx;
-
-            if(nxt_st.move_num > 10) return -1;
-            if(board[By][Bx] == '0') continue;
-            if(board[Ry][Rx] == '0') return nxt_st.move_num;
-
-            q.push(nxt_st);
-            cout << nxt_st.Ry << " " << nxt_st.Rx << " " << nxt_st.By << " " << nxt_st.Bx << " " << endl;
-        }
-        q.pop();
-    } // while
-
-    return -1;
-} // bfs()
-
+    return result;
+}
 
 int main() {
+    int N, M;
     cin >> N >> M;
-
     for(int i = 0; i < N; i++) {
         for(int j = 0; j < M; j++) {
             cin >> board[i][j];
             if(board[i][j] == 'R') {
-                R = make_pair(i, j);
                 board[i][j] = '.';
+                R_pos = {i, j};
             }
             else if(board[i][j] == 'B') {
-                B = make_pair(i, j);
                 board[i][j] = '.';
+                B_pos = {i, j};
             }
-        }
-    }
+       } // for j
+    } // for i
 
+    int ans = 0;
+    queue<pos> q;
+    q.push({R_pos, B_pos});
 
-    State st = {R.first, R.second, B.first, B.second, -1, 0};
-    cout << bfs(st);
+    while(!q.empty()) {
+        ans++;
+        if(ans > 10) break;
+
+        int size = q.size();
+        for(int h = 0; h < size; h++) {
+            pos cur_pos = q.front();
+            q.pop();
+            for(int i = 0; i < 4; i++) {
+                pos nxt_pos;
+                int flag = tilt(cur_pos, nxt_pos, i);
+                if(flag == 0) { // 성공
+                    // 프로그램 종료
+                    cout << ans;
+                    return 0;
+                } 
+                else if(flag == 1) { // 미정
+                    // q에 push
+                    q.push(nxt_pos);
+                }
+                else { // 실패
+                    // do nothing
+                }
+            } // for i
+        } // for h
+    } // while
+
+    cout << -1;
 
     return 0;
 }
